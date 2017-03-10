a09361c2e2604ddb7bfcf94e82e21db1ce3815a9
yuanxiulong <yuanxiulong@syswin.com>
2017-03-10 10:47:22 +0800

    xcode server 测试

---
+++ xcodeServer/xcodeServer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xcodeServer
+//
+//  Created by yuanxiulong on 2017/3/10.
+//  Copyright © 2017年 yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
